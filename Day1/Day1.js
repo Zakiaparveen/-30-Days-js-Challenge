e81ff0eccf66b5d7a843fca1f98b2c9d4442eb42
Samra321 <92590124+Zakiaparveen@users.noreply.github.com>
2024-07-20 16:16:35 +0500

    Add files via upload

---
+++ Day1/Day1.js
@@ -0,0 +1,61 @@
+
+
+//Activity 1 (Variable Declaration)
+// Task 1
+var a =4;
+console.log(a);
+
+//  Task 2
+let b =4;
+console.log(b);
+
+//Activity 2(Constant Declarition)
+//  Task 3
+ const c =true;
+console.log(c);
+
+
+//Activity 3(Data Type)
+//  Task 4
+
+ let b =4;
+let c=typeof(b)
+console.log(c);
+
+let d ="4";
+let c=typeof(d)
+console.log(c);
+
+let e =true;
+let c=typeof(e)
+
+console.log(c);
+
+let e ={ali:1,
+        hamza:2,
+        asma:3
+       };
+
+let c=typeof(e)
+console.log (typeof e);
+let arr =[0,1,2];         
+let c=typeof(e)
+console.log (typeof arr);
+
+
+//Activity 4(Reassinging a variable)
+//  Task 5
+ let c =4;
+c=6;
+let c=typeof(b)
+console.log(c);
+console.log(c);
+
+//Activity 5 (Understanding const)
+//Task 6
+const t=4;
+t=7;
+console.log(t);
+
+
+
