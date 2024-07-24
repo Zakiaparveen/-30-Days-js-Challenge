4684fbdd93db2b54c413a3b96decc9a5bd0c34f0
Samra321 <92590124+Zakiaparveen@users.noreply.github.com>
2024-07-24 13:01:03 +0500

    operator.js

--- Day2/operator.js
+++ Day2/operator.js
@@ -1,74 +1,74 @@
 // Activity 1
 // Task 1
-let num1_task1 = 10;
-let num2_task1 = 10;
-let sum_task1 = num1_task1 + num2_task1;
-console.log(sum_task1);
+let num1_1 = 10;
+let num2_1 = 10;
+let sum_1 = num1_1 + num2_1;
+console.log(sum_1);
 
 // Task 2
-let num3_task2 = 10;
-let num4_task2 = 10;
-let difference_task2 = num3_task2 - num4_task2;
-console.log(difference_task2);
+let num3_2 = 10;
+let num4_2 = 10;
+let difference_2 = num3_2 - num4_2;
+console.log(difference_2);
 
 // Task 3
-let num5_task3 = 10;
-let num6_task3 = 10;
-let product_task3 = num5_task3 * num6_task3;
-console.log(product_task3);
+let num5_3 = 10;
+let num6_3 = 10;
+let product_3 = num5_3 * num6_3;
+console.log(product_3);
 
 // Task 4
-let num7_task4 = 10;
-let num8_task4 = 10;
-let division_task4 = num7_task4 / num8_task4;
-console.log(division_task4);
+let num7_4 = 10;
+let num8_4 = 10;
+let division_4 = num7_4 / num8_4;
+console.log(division_4);
 
 // Task 5
-let num9_task5 = 10;
-let num10_task5 = 10;
-let modulus_task5 = num9_task5 % num10_task5;
-console.log(modulus_task5);
+let num9_5 = 10;
+let num10_5 = 10;
+let modulus_5 = num9_5 % num10_5;
+console.log(modulus_5);
 
 // Activity 2
 // Task 6
-let incrementMe_task6 = 7;
-incrementMe_task6++;
-console.log(incrementMe_task6);
+let incrementMe_6 = 7;
+incrementMe_6++;
+console.log(incrementMe_6);
 
 // Task 7
-let decrementMe_task7 = 7;
-decrementMe_task7--;
-console.log(decrementMe_task7);
+let decrementMe_7 = 7;
+decrementMe_7--;
+console.log(decrementMe_7);
 
 // Activity 3
 // Task 8
-let number1_task8 = 4;
-let number2_task8 = 6;
-if (number1_task8 > number2_task8) {
+let number1_8 = 4;
+let number2_8 = 6;
+if (number1_8 > number2_8) {
     console.log("Number 1 is greater");
-} else if (number2_task8 > number1_task8) {
+} else if (number2_8 > number1_8) {
     console.log("Number 2 is greater");
 } else {
     console.log("Both numbers are equal");
 }
 
 // Task 9
-let number1_task9 = 4;
-let number2_task9 = 6;
-if (number1_task9 >= number2_task9) {
+let number1_9 = 4;
+let number2_9 = 6;
+if (number1_9 >= number2_9) {
     console.log("Number 1 is greater");
-} else if (number1_task9 <= number2_task9) {
+} else if (number1_9 <= number2_9) {
     console.log("Number 2 is greater");
 } else {
     console.log("Both numbers are equal");
 }
 
 // Task 10
-let number1_task10 = "4";
-let number2_task10 = 4;
-if (number1_task10 === number2_task10) {
+let number1_10 = "4";
+let number2_10 = 4;
+if (number1_10 === number2_10) {
     console.log("Number 1 is strictly equal to Number 2");
-} else if (number1_task10 == number2_task10) {
+} else if (number1_10 == number2_10) {
     console.log("Number 1 is loosely equal to Number 2");
 } else {
     console.log("Both numbers are not equal");
@@ -76,27 +76,27 @@ if (number1_task10 === number2_task10) {
 
 // Activity 4
 // Task 11
-let num1_task11 = 14;
-let num2_task11 = 4;
-if (num1_task11 > num2_task11 && num1_task11 < num2_task11) {
+let num1_11 = 14;
+let num2_11 = 4;
+if (num1_11 > num2_11 && num1_11 < num2_11) {
     console.log("Both are same");
 } else {
     console.log("Both numbers are different");
 }
 
 // Task 12
-let num1_task12 = 14;
-let num2_task12 = 4;
-if (num1_task12 !== num2_task12) {
+let num1_12 = 14;
+let num2_12 = 4;
+if (num1_12 !== num2_12) {
     console.log("Both are different");
 } else {
     console.log("Both numbers are same");
 }
 
 // Task 13
-let num1_task13 = 14;
-let num2_task13 = 4;
-if (num1_task13 !== num2_task13) {
+let num1_13 = 14;
+let num2_13 = 4;
+if (num1_13 !== num2_13) {
     console.log("Both are different");
 } else {
     console.log("Both numbers are same");
@@ -104,6 +104,6 @@ if (num1_task13 !== num2_task13) {
 
 // Activity 5
 // Task 14
-let a_task14 = -3;
-let result_task14 = (a_task14 > 0) ? "positive" : "negative";
-console.log(result_task14);
+let a_14 = -3;
+let result_14 = (a_14 > 0) ? "positive" : "negative";
+console.log(result_14);
