feca21e604dd3f3d26b6a627587c16ff97cb5e36
Samra321 <92590124+Zakiaparveen@users.noreply.github.com>
2024-07-25 13:19:33 +0500

    Add files via upload

---
+++ Day 3/script.js
@@ -0,0 +1,80 @@
+// Activity
+// Task 2
+let age =18;
+if(age>=18){
+ console.log(" eligible")
+}else {
+  console.log(" not eligible")
+}
+
+// Activity 2
+// Task 3
+let a=10;
+let b=20;
+let c=30;
+if  (a >= b && a >= c){
+  console.log("a is greator");
+}else if(b>=a && b>=c){
+  console.log("b is greator");
+}else{
+  console.log("c is greator")
+}
+
+// Activty 3
+// Task 4
+let day = 3;
+switch(day){
+  case 1:
+console.log("saturday");
+break;
+  case 2:
+console.log("sunday");
+  case  3:
+console.log("monday")
+break;
+  default:
+    console.log("invalid day")
+    break;
+}
+
+
+// Activty 3
+// Task 5
+let score = 30;
+switch(score){
+  case 10:
+  
+console.log("A");
+break;
+  case 20:
+        
+console.log("B");
+    break;
+  case  30:
+    
+console.log("c")
+break;
+  default:
+    console.log("invalid day")
+    break;
+}
+
+// Task 6
+let k = 20;
+let res = k % 2 === 0 ? "even" : "odd";
+console.log(res);
+
+
+// Activity 5
+// task 7 
+
+let year=20;
+ if(year%4===0){
+   console.log("its leap year")
+ }
+else if(year%100===0){
+  console.log("its not leap year")
+}
+else{
+  console.log("not leap year")
+}
