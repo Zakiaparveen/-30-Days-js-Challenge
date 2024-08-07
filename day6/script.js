fe39dca21954034855429bde8914e98ec4f41dfb
Samra321 <92590124+Zakiaparveen@users.noreply.github.com>
2024-08-07 13:48:43 +0500

    Array.js

---
+++ day6/script.js
@@ -0,0 +1,61 @@
+let arr=[1,2,3,4,5]
+console.log(arr)
+
+// Task 2
+console.log(arr[0],arr[1])
+
+
+
+let arr=[1,2,3,4,5]
+arr.push(9)
+console.log(arr)
+
+
+
+let arr=[1,2,3,4,5]
+arr.pop()
+
+
+console.log(arr)
+
+
+let arr=[1,2,3,4,5]
+arr.unshift(2)
+
+
+console.log(arr)
+
+
+const numbers = [4, 9, 16, 25];
+const newArr = numbers.map(Math.sqrt)
+console.log(newArr);
+
+
+    let c =[0,1,2,3]
+for (let i=0;i<=c.length;i++){
+  console.log(arr[i]);
+}
+  
+
+
+const colors = ["red", "green", "blue", "yellow"];
+
+// colors.forEach(color => {
+//     console.log(color);
+// });
+
+colors.forEach(function(a))
+
+
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+  console.log(matrix[1][2]); 
+  
+
+
+
